7ee8700d4c0dbd43256fb9cbc7ba2be5f0aac8c7
mounika <mounikamusham12@gmail.com>
2021-12-14 18:22:01 +0530

    object

---
+++ object.js
@@ -0,0 +1,13 @@
+var car = {
+    color:"red",
+    company : "Maruthi Suzuki",
+    mileage:15,
+};
+console.log(car.color)
+
+
+var car2 = {};
+car2.color="blue";
+car2.company="hero";
+console.log(car2.company);
+console.log(car2);
